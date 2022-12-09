f54d1c99d5d1855fde34f6d8c5e12dc4cdf1d6f3
Kresten24 <ronie-8@hotmail.com>
2022-12-09 12:36:28 -0500

    fixed pop method implementation

--- doublyLinkedList/doublyLinkedList.js
+++ doublyLinkedList/doublyLinkedList.js
@@ -35,10 +35,11 @@ class DoublyLinkedList {
             if (this.length === 1) {
                 this.head = null;
                 this.tail = null;
+            }else{
+                this.tail = poppedNode.prev;
+                this.tail.next = null;
+                poppedNode.prev = null;
             }
-            this.tail = poppedNode.prev;
-            this.tail.next = null;
-            poppedNode.prev = null;
         }
         this.length--;
         return poppedNode;
